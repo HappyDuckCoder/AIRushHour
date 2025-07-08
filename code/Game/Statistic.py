9081c8eb4c18961d771c98668906edbdde8b14a3
duckilot <prto2802@gmail.com>
2025-07-08 23:08:05 +0700

    add task statistic

---
+++ code/Game/Statistic.py
@@ -0,0 +1,13 @@
+class Statistic: 
+    def __init__(self, time_executed, node_expand, total_node, total_cost): 
+        self.time_executed = 0
+        self.node_expand = []
+        self.total_node = 0
+        self.total_cost = 0
+    
+    # 4 set
+
+    # draw 
+
+    # update 
+
